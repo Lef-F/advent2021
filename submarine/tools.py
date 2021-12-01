53fe025d401ec510e3a05a3e616005a837b6b0d2
Lef Filippakis <Lef-F@users.noreply.github.com>
2021-12-01 16:40:35 +0100

    👾 some more docs

--- submarine/tools.py
+++ submarine/tools.py
@@ -4,18 +4,20 @@ import pandas as pd
 
 
 class Tools:
-    """Submarine basics"""
+    """Submarine basic toolset from IKEA"""
 
     def __init__(
         self,
         input,
         header_location: int = None,
-        column_names: str = None,
+        column_names: List[str] = None,
     ) -> None:
         """Connect your submarine tools to an input signal
 
         Args:
-            input (str): Path to input file
+            input (str): The path to the csv file with the input signal
+            header_location (int, optional): The row on which the csv header appears. Defaults to None.
+            column_names (List[str], optional): The name for the columns in the csv file, if they don't exist in it. Defaults to None.
         """
         self.input = input
         self.header_location = header_location
@@ -67,9 +69,71 @@ class Tools:
         return self.input_df.rolling(window).sum()
 
     def get_radar_step_directions(self) -> dict:
+        """Radar will crunch the numbers and return the pair of steps that were increments, decrements or other.
+
+        Examples:
+            If the signal would look as follows:
+            ```
+            199, NaN
+            200, increased
+            208, increased
+            210, increased
+            200, decreased
+            207, increased
+            240, increased
+            269, increased
+            260, decreased
+            263, increased
+            ```
+
+            Running `get_radar_step_directions` would return:
+            {'signal': {'increments': 7, 'decrements': 2, 'other': 1}}
+
+        Returns:
+            dict: The step statistics per column
+        """
         return self._diff(self.input_df)
 
     def get_windowed_radar_step_directions(self, window: int) -> dict:
+        """Configure the radar to perform a windowed aggregation of the steps that were increments, decrements or other.
+
+        Examples:
+            If the signal would look as follows:
+            ```
+            199
+            200
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
+            ```
+
+            Running `get_windowed_radar_step_directions` with `window=3` would process the input signal into:
+            ```
+            199, NaN, NaN
+            200, NaN, NaN
+            208, 607, NaN
+            210, 618, increments
+            200, 618, same
+            207, 617, decrements
+            240, 647, increments
+            269, 716, increments
+            260, 769, increments
+            263, 792, increments
+            ```
+            And then return:
+            {'signal': {'increments': 6, 'decrements': 1, 'other': 1}}
+
+        Args:
+            window (int): The size of the window in rows
+
+        Returns:
+            dict: The step statistics per column
+        """
         return self._diff(self._rolling_sum(window=window))
 
 
