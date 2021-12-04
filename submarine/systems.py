7f453cf83b27ad7053595d894b53b43cef65f606
Lef Filippakis <lef.filippakis@northvolt.com>
2021-12-04 22:56:39 +0100

    💃 return Series if squeeze is on

--- submarine/systems.py
+++ submarine/systems.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -13,6 +13,7 @@ class InputSignal:
         header_location: int = None,
         column_names: List[str] = None,
         data_type: type = None,
+        squeeze: bool = False,
     ) -> None:
         """Connect your submarine tools to an input signal
 
@@ -22,13 +23,14 @@ class InputSignal:
             column_names (List[str], optional): The name for the columns in the csv file, if they don't exist in it. Defaults to None.
             data_type (type, optional): The data type (if known) of the input data. Defaults to None.
                 Read more at https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
+            squeeze (bool, optional): Attempt to return a pandas Series if the input has only one column.
         """
         self.input = input
-        self.header_location = header_location
         self.input_df = self._input_loader(
             header_location=header_location,
             column_names=column_names,
             data_type=data_type,
+            squeeze=squeeze,
         )
         self.input_shape = self.input_df.shape
 
@@ -37,12 +39,14 @@ class InputSignal:
         header_location: int,
         column_names: List[str],
         data_type: type,
-    ) -> pd.DataFrame:
+        squeeze: bool = False,
+    ) -> Union[pd.Series, pd.DataFrame]:
         return pd.read_csv(
             self.input,
             header=header_location,
             names=column_names,
             dtype=data_type,
+            squeeze=squeeze,
         )
 
 
